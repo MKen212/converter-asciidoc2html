b69dbbfb81ec79d6ffe14ac36d5796033eb212c4
MarkKensington <46496040+MarkKensington@users.noreply.github.com>
2019-04-06 17:47:06 +0200

    first file conversion

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # converter-asciidoc2html
 Tool to convert asciidoc files to html using asciidoctor
+
+Activities to convert into HTML first time:
+* Using GitBash, clone repository from GitHub: git clone {repository}
+* List all asciidocs in main directory & sub-directories: ls -1d *.asciidoc */*.asciidoc
+* Create index.js to convert all asciidoc files to html format
+* Remove any existing files from ./output-html
+* Using Node.js Terminal, convert docs to html format: node {indexfile}
+* Copy "asciidoc.css" from main directory to ./output-html and any generated sub-directories
+* Copy any {additionalFiles} from repository into ./output-html
+* Check html files and make any required adjustments
+* Move contents of ./output-html directory and sub-directories to final destination
+
+Activities to update HTML subsequent times:
+* Using GitBash, update files from GitHub: git pull {repository}
+* (Using Node.js Terminal, ensure asciidoctor is up-to-date: npm update)
+* List all asciidocs in main directory & sub-directories: ls -1d *.asciidoc */*.asciidoc
+* Update relevant index.js with any changes
+* Remove any existing files from ./output-html
+* Using Node.js Terminal, convert docs to html format: node {indexfile}
+* Copy "asciidoc.css" from main directory to /output-html and any generated sub-directories
+* Copy any {additionalFiles} from repository into ./output-html
+* Check html files and make any required adjustments
+* Move contents of ./output-html directory and sub-directories to final destination
+
+
+
+Repository-specific information:
+
+ethereumbook
+* Repository: git@github.com:ethereumbook/ethereumbook.git
+* Directory: C:/Users/Mark/Documents/GitHubRepositories/ethereumbook/
+* indexfile: index-ethbook.js
+* AdditionalFiles: *.html; /images; /misc; /tools
+* Update book.html: location of "Index" heading & replace all .asciidoc with .html references
+* Final Destination: \Malarena SA\Blockchain\Ethereum\ethereumbook
+
+
---
+++ index-ethbook.js
@@ -0,0 +1,65 @@
+/* Converter using asciidoctor to convert asciidoc files to html
+ *
+ * ethereumbook repository
+ *
+ * Repository: git@github.com:ethereumbook/ethereumbook.git
+ * Directory: C:/Users/Mark/Documents/GitHubRepositories/ethereumbook/
+ * 
+*/
+const asciidoctor = require('asciidoctor')();
+
+function convFile (source, destn) {
+  asciidoctor.convertFile(source, { "to_dir": destn });
+  console.log(`${source} saved to ${destn}`);
+}
+
+let sourceDir = "C:/Users/Mark/Documents/GitHubRepositories/ethereumbook/";
+let destn1 = "./output-html";
+let destn2 = "./output-html/contrib";  // extra sub-directory required for this repo
+
+/* Format for each file is: convFile(sourceDir + "filename", destnation); */
+
+convFile(sourceDir + "01what-is.asciidoc", destn1);
+convFile(sourceDir + "02intro.asciidoc", destn1);
+convFile(sourceDir + "03clients.asciidoc", destn1);
+convFile(sourceDir + "04keys-addresses.asciidoc", destn1);
+convFile(sourceDir + "05wallets.asciidoc", destn1);
+convFile(sourceDir + "06transactions.asciidoc", destn1);
+convFile(sourceDir + "07smart-contracts-solidity.asciidoc", destn1);
+convFile(sourceDir + "08smart-contracts-vyper.asciidoc", destn1);
+convFile(sourceDir + "09smart-contracts-security.asciidoc", destn1);
+convFile(sourceDir + "10tokens.asciidoc", destn1);
+convFile(sourceDir + "11oracles.asciidoc", destn1);
+convFile(sourceDir + "12dapps.asciidoc", destn1);
+convFile(sourceDir + "13evm.asciidoc", destn1);
+convFile(sourceDir + "14consensus.asciidoc", destn1);
+convFile(sourceDir + "appdx-dev-tools.asciidoc", destn1);
+convFile(sourceDir + "appdx-evm-opcodes-gas.asciidoc", destn1);
+convFile(sourceDir + "appdx-forks-history.asciidoc", destn1);
+convFile(sourceDir + "appdx-shortlinks.asciidoc", destn1);
+convFile(sourceDir + "appdx-standards-eip-erc.asciidoc", destn1);
+convFile(sourceDir + "appdx-web3js-tutorial.asciidoc", destn1);
+convFile(sourceDir + "book.asciidoc", destn1);
+
+convFile(sourceDir + "contrib/angular4-truffle.asciidoc", destn2);
+convFile(sourceDir + "contrib/aws-network-operation.asciidoc", destn2);
+convFile(sourceDir + "contrib/aws-network-setup.asciidoc", destn2);
+convFile(sourceDir + "contrib/aws-setup.asciidoc", destn2);
+convFile(sourceDir + "contrib/design-patterns.asciidoc", destn2);
+convFile(sourceDir + "contrib/devp2p-protocol.asciidoc", destn2);
+convFile(sourceDir + "contrib/etc-history.asciidoc", destn2);
+convFile(sourceDir + "contrib/ethereum-basics.asciidoc", destn2);
+convFile(sourceDir + "contrib/ethereum-testnets.asciidoc", destn2);
+convFile(sourceDir + "contrib/example.asciidoc", destn2);
+convFile(sourceDir + "contrib/google-cloud-testnet.asciidoc", destn2);
+convFile(sourceDir + "contrib/governance.asciidoc", destn2);
+convFile(sourceDir + "contrib/node-communication-interaction.asciidoc", destn2);
+convFile(sourceDir + "contrib/privacy-enhancing-patterns.asciidoc", destn2);
+convFile(sourceDir + "contrib/scaling.asciidoc", destn2);
+convFile(sourceDir + "contrib/upgradability-patterns.asciidoc", destn2);
+convFile(sourceDir + "contrib/using-puppeth.asciidoc", destn2);
+
+convFile(sourceDir + "glossary.asciidoc", destn1);
+convFile(sourceDir + "preface.asciidoc", destn1);
+
+console.log("All Converted!");
